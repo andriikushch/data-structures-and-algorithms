1c4ea9c52c6d50cfae38afa44785bf78763d8bf4
Andrii Kushch <andrii.kushch@gmail.com>
2019-09-24 21:52:27 +0200

    fix linked list

--- data-structures/linked_list.cpp
+++ data-structures/linked_list.cpp
@@ -3,141 +3,162 @@ using namespace std;
 
 struct node
 {
+  node(int key) {
+        this->key = key;
+        this->next = nullptr;
+  }
   int key;
   node * next;
 };
 
-void print_list(node * begin);
-int search_list(node * begin, int val);
-node * insert(node * begin, int position, node * new_item);
-node * remove(node * begin, int position);
+// todo: add tail
+struct linked_list
+{
+  node * head;
+
+  linked_list() {
+    this->head = nullptr;
+  }
+
+  void remove(int position) {
+    int i = 0;
+
+    node * current_node = this->head;
+    node * prev_node;
+
+    while (current_node != nullptr)
+    {
+      // remove from the beginning
+      if (position == 0) {
+          this->head = current_node->next;
+          current_node = nullptr;
+
+          return;
+      }
+
+      // remove from the middle
+      if (i == position && current_node->next != nullptr) {
+          prev_node->next = current_node->next;
+          current_node = nullptr;
+          return;
+      }
+
+      // remove from the end
+      if (i == position && current_node->next == nullptr) {
+          prev_node->next = nullptr;
+          current_node = nullptr;
+
+          return;
+      }
+
+      prev_node = current_node;
+      current_node = current_node->next;
+
+      i++;
+    }
+
+    return;
+  }
+
+  void insert(int position, node * new_item) {
+    // insert to the beginning
+    if (position == 0 && this->head == nullptr) {
+      this->head = new_item;
+      return;
+    } else if (position == 0)
+    {
+      new_item->next = this->head;
+      this->head = new_item;
+      return;
+    }
+
+    int i = 0;
+
+    node * current_node = this->head;
+    node * prev_node;
+
+    while (current_node != nullptr)
+    {
+      // insert in the middle
+      if (i == position) {
+          new_item->next = current_node;
+          prev_node->next = new_item;
+
+          return;
+      }
+
+      prev_node = current_node;
+      current_node = current_node->next;
+
+      i++;
+    }
+
+    // insert to the end
+    if (i == position && prev_node->next == nullptr) {
+      prev_node->next = new_item;
+    }
+
+    return;
+  }
+
+  int search_list(int val) {
+    int i = 0;
+    node * current_node = this->head;
+
+    while (current_node != nullptr)
+    {
+      if (current_node->key == val) {
+          return i;
+      }
+      current_node = current_node->next;
+      i++;
+    }
+
+    return -1;    
+  }
+
+  void print() {
+    node * current_node = this->head;
+    cout << endl << "#######START##########" << endl;
+    while (current_node != nullptr)
+    {
+      cout << current_node->key << endl;
+      current_node = current_node->next;
+    }
+    cout << "########END############" << endl;
+  }
+
+};
 
 int main() {
-  node n1;
-  n1.key = 1;
-  n1.next = nullptr;
+  node * n1 = new node(1);
+  node * n2 = new node(2);
+  node * n3 = new node(3);
+  node * n4 = new node(4);
+  node * n5 = new node(5);
+  node * n6 = new node(6);
+  node * n7 = new node(7);
 
-  node * head = &n1;
+  linked_list * ll = new linked_list();
 
-  node n2;
-  n2.key = 2;
-  n2.next = nullptr;
+  ll->insert(0, n1);
+  ll->insert(1, n2);
+  ll->insert(2, n3);
+  ll->insert(0, n4);
+  ll->insert(4, n5);
+  ll->insert(0, n6);
+  ll->insert(2, n7);
+  ll->print(); // 6,4,7,1,2,3,5,
 
-  node n3;
-  n3.key = 3;
-  n3.next = nullptr;
+  delete n1;
+  delete n2;
+  delete n3;
+  delete n4;
+  delete n5;
+  delete n6;
+  delete n7;
 
-  n1.next = &n2;
-  n2.next = &n3;
-
-  node n4;
-  n4.key = 4;
-  n4.next = nullptr;
-
-  head = insert(head, 3, &n4);
-  print_list(head);
-
-  head = remove(head, 1);
-  print_list(head);
-
+  delete ll;
 
   return 0;
-}
-
-node * remove(node * current_node, int position) {
-  int i = 0;
-
-  node * head = current_node;
-  node * prev_node;
-
-  while (current_node != nullptr)
-  {
-    // remove from the beginning
-    if (position == 0) {
-        head = current_node->next;
-        current_node = nullptr;
-
-        return head;
-    }
-
-    // remove from the middle
-    if (i == position && current_node->next != nullptr) {
-        prev_node->next = current_node->next;
-        current_node = nullptr;
-        return head;
-    }
-
-     // remove from the end
-    if (i == position && current_node->next == nullptr) {
-        prev_node->next = nullptr;
-        current_node = nullptr;
-
-        return head;
-    }
-
-    prev_node = current_node;
-    current_node = current_node->next;
-
-    i++;
-  }
-
-  return head;
-}
-
-node * insert(node * current_node, int position, node * new_item) {
-  int i = 0;
-
-  node * head = current_node;
-  node * prev_node;
-
-  while (current_node != nullptr)
-  {
-    // insert to the beginning
-    if (position == 0) {
-        new_item->next = current_node;
-        return new_item;
-    }
-    
-    // insert in the middle
-    if (i == position) {
-        new_item->next = current_node;
-        prev_node->next = new_item;
-
-        return head;
-    }
-
-    prev_node = current_node;
-    current_node = current_node->next;
-
-    i++;
-  }
-
-  // insert to the end
-  if (i == position && prev_node->next == nullptr) {
-    prev_node->next = new_item;
-  }
-
-  return head;
-}
-
-int search_list(node * current_node, int val) {
-  int i = 0;
-  while (current_node != nullptr)
-  {
-    if (current_node->key == val) {
-        return i;
-    }
-    current_node = current_node->next;
-    i++;
-  }
-
-  return -1;    
-}
-
-void print_list(node * current_node) {
-  while (current_node != nullptr)
-  {
-    cout << current_node->key << endl;
-    current_node = current_node->next;
-  }
 }
